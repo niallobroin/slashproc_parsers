cf314be7cf88f2d3bb3fd03755e7d932ac618419
Niall OByrnes <nobyrnes@icloud.com>
2015-02-23 23:59:02 +0300

    Added some desc to cpuinfo

--- parsers/cpuinfo.py
+++ parsers/cpuinfo.py
@@ -41,7 +41,21 @@ class CpuInfo(BasicSPParser):
             thevars[CpuInfo.key_format(i)] = {'name': i,
                                               'unit': '',
                                               'parents': parents}
+
+
         # TODO Add desc to every entry here 
+        thevars['processor']['desc'] = "Provides each processor with an identifying number. On systems that have one processor, only a 0 is present."
+        thevars['cpu_family']['desc'] =  "Authoritatively identifies the type of processor in the system. For an Intel-based system, place the number in front of '86' to determine the value. This is particularly helpful for those attempting to identify the architecture of an older system such as a 586, 486, or 386. Because some RPM packages are compiled for each of these particular architectures, this value also helps users determine which packages to install."
+        thevars['model_name']['desc'] = "Displays the common name of the processor, including its project name."
+        thevars['cpu_mhz']['desc'] = "Shows the precise speed in megahertz for the processor to the thousandths decimal place."
+        thevars['cpu_mhz']['unit'] = 'MHz'
+        thevars['cache_size']['desc'] = "Displays the amount of level 2 memory cache available to the processor."
+        thevars['cache_size']['unit'] = 'KB'
+        thevars['siblings']['desc'] = "Displays the number of sibling CPUs on the same physical CPU for architectures which use hyper-threading."
+        thevars['flags']['desc'] = "Defines a number of different qualities about the processor, such as the presence of a floating point unit (FPU) and the ability to process MMX instructions."
+
+
+
 
 
         return thevars
@@ -74,7 +88,10 @@ class CpuInfo(BasicSPParser):
                     processor_num =  'core' + v
                     retdict['core'][processor_num] = dict()
 
+
                 retdict['core'][processor_num][k] = v
+                if k == 'cache_size':
+                    retdict['core'][processor_num]['cache_size'] = v.strip()[0]
 
         return retdict
 

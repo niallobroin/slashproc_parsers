90a4da4dc0ccd583939c4d74f380bb93608d677c
Niall OByrnes <nobyrnes@icloud.com>
2015-03-03 09:13:42 +0000

    refactor cmdline.py edited online with Bitbucket

--- parsers/cmdline.py
+++ parsers/cmdline.py
@@ -31,30 +31,33 @@ class CmdLine(BasicSPParser):
             Returns: thevars (dict): variables
         """
         thevars = {
-                'ro': {'desc': "Indicates that the kernel is mounted read-only",
-                       'label': 'Read Only',
-                       'unit': '',
-                       'parents': ['cmdline'],},
+            'raw': {'desc': "The full kernel boot command",
+                    'label': "Command Kernel Line",
+                    'parents': ['cmdline'],},
 
-                'root': {'desc': "Location of the root filesystem image",
-                         'label': 'Root directory',
-                         'unit': '',
-                         'parents': ['cmdline'],},
+            'boot_image':{'desc': '',
+                          'label': "Boot Image",
+                          'parents': ['cmdline'],},
+
+            'kernel_perm': {'desc': "Permissions on the kernel",
+                            'label': "Kernel Permissions",
+                            'parents': ['cmdline'],},
+
+            'root': {'desc': "Location of the root filesystem image",
+                     'label': 'Root directory',
+                     'parents': ['cmdline'],},
+            
+            'some_name': {'desc': "Red Hat Graphical Boot. Graphical booting is supported",
+                     'label': 'Graphical Boot',
+                     'parents': ['cmdline'],},
+            
+            'kernel_msg': {'desc': "All verbose kernel messages except extremely serious should be suppressed at boot time",
+                           'label': 'Suppress boot messages',
+                           'parents': ['cmdline'],},
                 
-                'rhgb': {'desc': "Red Hat Graphical Boot. Graphical booting is supported",
-                         'label': 'Graphical Boot',
-                         'unit': '',
-                         'parents': ['cmdline'],},
-                
-                'quiet': {'desc': "All verbose kernel messages except extremely serious should be suppressed at boot time",
-                         'label': 'Suppress boot messages',
-                         'unit': '',
-                         'parents': ['cmdline'],},
-                
-                'spare': {'desc': "Spare",
-                         'label': 'Spare',
-                         'unit': '',
-                         'parents': ['cmdline'],},
+            'lang': {'desc': "Language",
+                     'label': 'Language',
+                     'parents': ['cmdline'],},
                 }
         return thevars
 
@@ -64,15 +67,17 @@ class CmdLine(BasicSPParser):
 
             Returns: stats (dict): dictionary with variables and their values
         """
-        for l in open(CmdLine.CMDLINE):
-            line = l.split()
+        with open(CmdLine.CMDLINE) as l:
+            raw = l.read()
 
-            cmdline_data = {"ro": line[0],
-                            "root": line[1].strip().replace('root=', ''),
-                            "rhgb": line[2],
-                            "quiet": line[3],
-                            "spare": line[4]}
-        return {'cmdline': cmdline_data}
+        line = raw.split()
+
+        data = {"raw": raw,
+                "kernel_perm": line[0],
+                "root": line[1].strip().replace('root=', ''),
+                }
+
+        return {'cmdline': data}
 
 if __name__ == "__main__":
     cl = CmdLine()
